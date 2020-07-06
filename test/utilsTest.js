b3ccc52d524f5350614436a362cff3d0c0de03a9
matheuslyra@gmail.com <matheuslyra@gmail.com>
2020-07-06 01:53:04 -0300

    Utils Tests TDD

--- test/utilsTest.js
+++ test/utilsTest.js
@@ -1,24 +1,43 @@
-const {assert} = require('chai');
-const {userName, addNumbers} = require('../utils');
+const chai = require('chai');
+const utils = require('../utils');
 
-describe('Utils Tests', ()=>{
-    it('Should return the username',()=>{
-        assert.equal(userName(), 'Matheus');
+describe('Util Tests',()=>{
+    it('Should return the reference title',()=>{
+        chai.assert.equal(utils.getData('8710126', 'title'), 'MEC and IoT Based Automatic Agent Reconfiguration in Industry 4.0');
+    });
+    it('Should return the right number of filtered elements',()=>{
+        chai.assert.equal(utils.filterItems(13),3);
+        chai.assert.equal(utils.filterItems(14),2);
+        chai.assert.equal(utils.filterItems(0),6);
+    });
+    it('Should return the sum of the items over a certain price',()=>{
+        chai.assert.equal(utils.sumItems(13), 44.97);
+        chai.assert.equal(utils.sumItems(14), 30.98);
+        chai.assert.equal(utils.sumItems(0), 78.94);
+    });
+    it('Should return the price with discount', ()=>{
+        chai.assert.equal(utils.productDiscount('arrival',0.2),10.392);
+        chai.assert.equal(utils.productDiscount('o hobbit',0.15),11.8915);
     });
 
-    it('Should return an output of the type String', ()=>{
-        const result = userName();
-        assert.typeOf(result, 'string');
-    });
+    it('Should return the items over price',()=>{
+        chai.assert.isArray(utils.returnItemsOverPrice(0));
+        chai.assert.equal(utils.returnItemsOverPrice(12.00).length, 4);
+        chai.assert.equal(utils.returnItemsOverPrice(14.00).length, 2);
+    })
 
-    it('addNumbers should return a value above 5', ()=>{
-        const result = addNumbers(2,3);
-        assert.isAbove(result, 5);
-    });
+    it('Should return the item price by name', ()=>{
+        chai.assert.equal(utils.returnPrice('fundação'), 15.99);
+        chai.assert.equal(utils.returnPrice('lord of the rings'), 10.99);
+    })
 
-    it('addNumbers should return a Number', ()=>{
-        const result = addNumbers(2,3);
-        assert.typeOf(result, 'Number');
-    });
-
+    it('Should return if found a value using binary search', ()=>{
+        chai.assert.equal(utils.binarySearch([22, 34, 51, 81, 12, 13, 20, 7],0,7,51), 'a');
+        /*chai.assert.equal(utils.binarySearch(array,0,8,12), 1);
+        chai.assert.equal(utils.binarySearch(array,0,8,81), 1);
+        chai.assert.equal(utils.binarySearch(array,0,8,3), 1);
+        chai.assert.equal(utils.binarySearch(array,0,8,88), 0);
+        chai.assert.equal(utils.binarySearch(array,0,8,42), 0);
+        chai.assert.equal(utils.binarySearch(array,0,8,0), 0);*/
+    })
 });
--- utils.js
+++ utils.js
@@ -1,10 +1,80 @@
-function userName(){
-    return 'Matheus';
+const fs = require('fs');
+
+const items = [
+    {name: 'watchmen', price:14.99},
+    {name: 'arrival', price:12.99},
+    {name: 'lord of the rings', price:10.99},
+    {name: 'fundação', price:15.99},
+    {name: 'o hobbit', price:13.99},
+    {name: 'narnia', price:9.99}
+];
+
+function getData(reference, field){
+    const file = JSON.parse(fs.readFileSync('./input.json'));
+    var result = 0;
+
+    file.forEach(element => {
+        if(element.literature == reference)
+            result = element.title;
+    });
+
+    return result;
 }
 
-function addNumbers(a,b){
-    return (a + b + 5);
+//console.log(items.map(a=>a.price*100));
+
+function filterItems(price){ //return the number of filtered items
+    return items.filter(a=>a.price>price).length;
 }
 
-exports.userName = userName;
-exports.addNumbers = addNumbers;
+function sumItems(price){
+    return items.filter(a=>a.price>price).reduce((total, a)=>{
+        return a.price + total;
+    },0);
+}
+
+function productDiscount(productName, discount){
+    const product = items.find(a=>a.name===productName);
+    return  product.price - (product.price * discount);
+}
+
+function returnItemsOverPrice(price){
+    return items.filter(a=>a.price>price);
+}
+
+function returnPrice(name){
+    return items.find(a=>a.name === name).price;
+}
+
+function localBinarySearch(array, left, right, value){
+    middle = Math.trunc((left+right)/2);
+    if(left<=right){
+        if(array[middle]===value) {
+            return "a";
+        }
+        else if (value < array[middle]){
+            localBinarySearch(array, left, middle-1, value);
+        }else {
+            localBinarySearch(array, middle+1, right, value);
+        }
+    }else{
+        return "b";
+    }
+}
+
+function binarySearch(array, left, right, value){
+    array.sort((a,b)=>{
+        return a-b;
+    })
+
+    return localBinarySearch(array, left, right, value);
+   
+}  
+
+exports.getData = getData;
+exports.filterItems = filterItems;
+exports.sumItems = sumItems;
+exports.productDiscount = productDiscount;
+exports.returnItemsOverPrice = returnItemsOverPrice;
+exports.returnPrice = returnPrice;
+exports.binarySearch = binarySearch;
